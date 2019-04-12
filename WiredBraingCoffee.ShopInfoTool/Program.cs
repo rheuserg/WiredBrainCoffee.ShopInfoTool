8531cca2208e1181a953d7133b7eb3e9ea357a6a
Robert Heuser <robert.heuser@contractorconnection.com>
2019-04-12 10:48:32 -0400

    Add project files.

---
+++ WiredBraingCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredBraingCoffee.ShopInfoTool
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Wired Brain Coffee Shop Info Tool!");
+		}
+	}
+}
